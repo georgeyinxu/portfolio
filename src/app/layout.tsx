bf441ab4af4fc461e6363ad3e7ac4e73af3f8d32
George Yin <georgeyinxu@gmail.com>
2024-05-20 13:35:58 +0800

    feat: :lipstick: completed base ui for the home page of the portfolio
    completed base ui for the home page of the portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 import { ReactQueryProvider } from '@/providers';
 
-const inter = Inter({ subsets: ['latin'] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 export const metadata: Metadata = {
   title: "George's Portfolio",
@@ -17,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900 ${poppins.className}`}
+      >
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        poppins: ['var(--poppins)'],
+      },
+      colors: {
+        navy: {
+          DEFAULT: '#0a192f',
+          light: '#112240',
+          lightest: '#233554',
+        },
+        slate: {
+          DEFAULT: '#8892b0',
+          light: '#a8b2d1',
+          lightest: '#ccd6f6',
+        },
+        white: '#e6f1ff',
+        green: '#64ffda',
       },
     },
   },
